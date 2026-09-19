ad3419593957ccc0266096f6cccdbed7043c0bb0
Kaito Udagawa <umireon@kaito.tokyo>
2026-09-19 13:06:43 +0900

    Limit chunk evaluation root
    Signed-off-by: Kaito Udagawa <umireon@kaito.tokyo>

--- src/rnnoise_mlx/training_cuda/loop.py
+++ src/rnnoise_mlx/training_cuda/loop.py
@@ -115,7 +115,7 @@ class CUDATrainingLoop:
         )
         self.optimizer.update(self.chunk, gradients)
         loss = accumulated_loss / target_frames
-        mx.eval(loss, state, gradients)
+        mx.eval(loss)
         return CudaUpdateResult(
             loss=loss,
             state=state,
